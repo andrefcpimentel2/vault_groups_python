20af6bf1cca18acd4a9b58e89664950a01451e94
Guy Barros <38210175+GuyBarros@users.noreply.github.com>
2019-07-19 12:54:43 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sudo apt-get install python3-pip -y && pip3 install hvac && python3 ./create_groups.py
+echo demo | sudo -S apt-get install python3-pip -y && pip3 install hvac && python3 ./create_groups.py
